7ac760488b6a4e1dd95c7fb5f43a97ec043e9faa
Artem-Rzhankoff <artemr2004@rambler.ru>
2024-02-24 21:10:07 +0300

    feat: Implement simple concurrent stack

---
+++ TreiberStack/TreiberStack/Node.cs
@@ -0,0 +1,19 @@
+﻿namespace TreiberStack;
+
+public class Node<T>
+{
+    public T Data;
+    public Node<T>? Next;
+
+    public Node(T data)
+    {
+        Data = data;
+        Next = null;
+    }
+
+    public Node(T data, Node<T> next)
+    {
+        Data = data;
+        Next = next;
+    }
+}
