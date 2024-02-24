784987262d6e9c4c8970fc270ff93f2f15d6cac2
Artem-Rzhankoff <artemr2004@rambler.ru>
2024-02-24 21:10:07 +0300

    feat: Implement simple concurrent stack

---
+++ TreiberStack/TreiberStack/MyConcurrentStack.cs
@@ -0,0 +1,57 @@
+﻿using static System.Threading.Interlocked;
+
+namespace TreiberStack;
+
+public class MyConcurrentStack<T>
+{
+
+    private Node<T> _head;
+
+    private static bool CompareAndSet(ref Node<T> value, Node<T> expectedValue, Node<T> newValue)
+    {
+        if (EqualityComparer<T>.Default.Equals(value.Data, expectedValue.Data))
+        {
+            value = newValue;
+            return true;
+        }
+
+        return false;
+    }
+    
+    public T Pop()
+    {
+        while (true)
+        {
+            var localHead = _head;
+            if (CompareAndSet(ref _head, localHead, localHead.Next))
+            {
+                return localHead.Data;
+            }
+            
+        }
+    }
+
+    public void Push(T value)
+    {
+        while (true)
+        {
+            var localHead = _head;
+            var newNode = new Node<T>(value, localHead);
+            if (CompareAndSet(ref _head, localHead, newNode))
+            {
+                return;
+            }
+        }
+    }
+
+    public T TopElement()
+    {
+        var localHead = _head;
+        if (localHead == null)
+        {
+            throw new NullReferenceException();
+        }
+
+        return localHead.Data;
+    }
+}
