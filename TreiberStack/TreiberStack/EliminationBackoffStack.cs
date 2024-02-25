7cec07a81bfa2b0ef155b4051c8631a4e9c7474b
Artem-Rzhankoff <artemr2004@rambler.ru>
2024-02-25 19:53:44 +0300

    feat: implement elimination backoff stack

---
+++ TreiberStack/TreiberStack/EliminationBackoffStack.cs
@@ -0,0 +1,53 @@
+﻿namespace TreiberStack;
+
+public class EliminationBackoffStack<T> : MyConcurrentStack<T> where T : class
+{
+    private const int Capacity = 100;
+    private EliminationArray<T> _eliminationArray = new EliminationArray<T>(Capacity, 10);
+
+    public override void Push(T value)
+    {
+        var newNode = new Node<T>(value);
+        
+        while (true)
+        {
+            if (TryPush(newNode))
+            {
+                return;
+            }
+
+            try
+            {
+                var otherValue = _eliminationArray.Visit(value, 10);
+                // гарантирует, что обмен был осуществлен с pop
+                if (otherValue == null)
+                {
+                    return;
+                }
+            }
+            catch (TimeoutException) { }
+        }
+    }
+
+    public override T Pop()
+    {
+        while (true)
+        {
+            var returnNode = TryPop();
+            if (returnNode != null)
+            {
+                return returnNode.Data;
+            }
+
+            try
+            {
+                var otherValue = _eliminationArray.Visit(null, 10);
+                if (otherValue != null)
+                {
+                    return otherValue;
+                }
+            }
+            catch (TimeoutException) { }
+        }
+    }
+}
---
+++ TreiberStack/TreiberStack/LockFreeExchanger.cs
@@ -0,0 +1,91 @@
+﻿using static System.Threading.Interlocked;
+
+namespace TreiberStack;
+
+public class StampedReference <T>
+{
+    public T? Item = default;
+    public int Stamp = 1;
+    public StampedReference() {}
+
+    public StampedReference(T? data, int stamp)
+    {
+        Item = data;
+        Stamp = stamp;
+    }
+}
+
+public class LockFreeExchanger<T>
+{
+    public StampedReference<T> Slot = new();
+    private const int EMPTY = 1;
+    private const int WAITING = 2;
+    private const int BUSY = 3;
+    
+    // AtomicStampedReference<T> slot = new AtomicStampedReference<T>(null, 0);
+
+    public T? MyExchange(T? myItem, long timeout)
+    {
+        var timeBound = DateTime.Now.Nanosecond + timeout;
+        int[] stampHolder = [EMPTY]; // тут не ссылка ли должна быть ??
+        while (true)
+        {
+            if (DateTime.Now.Nanosecond > timeBound)
+            {
+                throw new TimeoutException();
+            }
+
+            T yrItem = Slot.Item;
+            int stamp = stampHolder[0];
+            switch (stamp)
+            {
+                case EMPTY:
+                    var newStampedReference = new StampedReference<T>(myItem, WAITING);
+                    // 1. Add 1st value to slot
+                    // 2. Set its stamp as WAITING (for 2nd)
+                    if (CompareExchange(ref Slot, new StampedReference<T>(default, EMPTY),
+                            newStampedReference) == newStampedReference)
+                    {
+                        while (DateTime.Now.Nanosecond < timeBound)
+                        {
+                            var dateItem = Slot.Item;
+                            if (stampHolder[0] == BUSY)
+                            {
+                                // ожидающий поток -- единственный, кто может изменить состояние на empty
+                                Exchange(ref Slot, new StampedReference<T>(default, EMPTY));
+                                return dateItem;
+                            }
+                        }
+
+                        var foo = new StampedReference<T>(default, EMPTY);
+
+                        if (CompareExchange(ref Slot, newStampedReference, foo) == foo)
+                        {
+                            throw new TimeoutException();
+                        }
+                        else
+                        {
+                            // это значит, что другой поток перевел состояние WAITING в BUSY
+                            var dateItem = Slot.Item;
+                            Exchange(ref Slot, new StampedReference<T>(default, EMPTY));
+                            return dateItem;
+                        }
+                    }
+                    break;
+                case WAITING:
+                    var newReference = new StampedReference<T>(myItem, BUSY);
+                    if (CompareExchange(ref Slot, new StampedReference<T>(yrItem, WAITING), newReference) ==
+                        newReference)
+                    {
+                        return yrItem;
+                    }
+
+                    break;
+                case BUSY:
+                    break;
+            }
+        }
+        
+    }
+
+}
