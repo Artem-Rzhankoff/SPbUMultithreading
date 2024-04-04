17329625a54015ecb429a01e70f5bb4f862f831d
Artem-Rzhankoff <artemr2004@rambler.ru>
2024-04-04 18:01:05 +0300

    feat: add ContinueWith overload

--- MyThreadPool/MyThreadPoolLibrary/IMyTask.cs
+++ MyThreadPool/MyThreadPoolLibrary/IMyTask.cs
@@ -7,5 +7,7 @@ public interface IMyTask<TResult>
     public TResult Result { get;  }
 
     public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> continuation);
+    
+    public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<IMyTask<TResult>, TNewResult> continuation);
 
 }
